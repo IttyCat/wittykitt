8722d6dabd586d09bca7fd67930b6ab1a77ba074
IttyCat <138669763+IttyCat@users.noreply.github.com>
2024-01-11 00:40:42 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector("#menu-icon");
+let navlinks = document.querySelector(".navlinks");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    navlinks.classList.toggle("open");
+
+
+}
+    
